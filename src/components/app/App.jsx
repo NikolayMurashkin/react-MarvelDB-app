ee875978423a3ad196160687b73745cc497a6862
Nikolay <m.nikolay1990@gmail.com>
2022-07-29 14:35:44 +0700

    fix deploy bugs

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,4 +1,4 @@
-import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import {HashRouter as Router, Routes, Route} from "react-router-dom";
 
 import AppHeader from "../appHeader/AppHeader";
 import {MainPage, ComicsPage, SingleComic, Page404} from '../pages';
