42e2065b32a6b37e5a7267c625d5cfa379ee7a65
Nikolay <m.nikolay1990@gmail.com>
2022-08-02 17:09:48 +0700

    refactoring

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,7 +1,13 @@
 import {HashRouter as Router, Routes, Route} from "react-router-dom";
+import {lazy, Suspense} from "react";
 
 import AppHeader from "../appHeader/AppHeader";
-import {MainPage, ComicsPage, SingleComic, Page404} from '../pages';
+import Spinner from "../spinner/Spinner";
+
+const Page404 = lazy(() => import('../pages/page404/Page404').then(({default: Page404}) => ({default: Page404})));
+const MainPage = lazy(() => import('../pages/MainPage/MainPage').then(({default: MainPage}) => ({default: MainPage})));
+const ComicsPage = lazy(() => import('../pages/ComicsPage/ComicsPage').then(({default: ComicsPage}) => ({default: ComicsPage})));
+const SingleComic = lazy(() => import('../pages/singleComicPage/singleComic/SingleComic').then(({default: SingleComic}) => ({default: SingleComic})));
 
 const App = () => {
     return (
@@ -9,12 +15,14 @@ const App = () => {
             <div className="app">
                 <AppHeader/>
                 <main>
-                    <Routes>
-                        <Route exact path="/" element={<MainPage/>}/>
-                        <Route path="/comics" element={<ComicsPage/>}/>
-                        <Route path="/comics/:comicId" element={<SingleComic/>}/>
-                        <Route path="*" element={<Page404/>}/>
-                    </Routes>
+                    <Suspense fallback={<Spinner/>}>
+                        <Routes>
+                            <Route exact path="/" element={<MainPage/>}/>
+                            <Route path="/comics" element={<ComicsPage/>}/>
+                            <Route path="/comics/:comicId" element={<SingleComic/>}/>
+                            <Route path="*" element={<Page404/>}/>
+                        </Routes>
+                    </Suspense>
                 </main>
             </div>
         </Router>
